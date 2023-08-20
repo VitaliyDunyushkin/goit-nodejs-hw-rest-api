2aefa22c6cc85c38e13358c47a7152768c5cb004
VitaliyDunyushkin <117518206+VitaliyDunyushkin@users.noreply.github.com>
2023-08-20 19:20:39 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,49 +1,59 @@
 const express = require("express");
 const joi = require("joi");
-const Contact = require("../../models/contact");
+const Contact = require("../../models/contact.js");
 
-// const contactShema = joi.object({
-//   name: joi.string().required(),
-//   email: joi.string().email().required(),
-//   phone: joi.string().required(),
-// });
+const contactShema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  phone: joi.string().required(),
+  favorite: joi.boolean().default(false),
+});
 
-// const contactPutShema = joi.object({
-//   name: joi.string(),
-//   email: joi.string().email(),
-//   phone: joi.string(),
-// });
+const contactPutShema = joi.object({
+  name: joi.string(),
+  email: joi.string().email(),
+  phone: joi.string(),
+  favorite: joi.boolean(),
+});
+
+const contactUpdateFavoriteSchema = joi.object({
+  favorite: joi.boolean().required(),
+});
 
 const router = express.Router();
 
-// router.get("/", async (req, res, next) => {
-//   const result = await contacts.listContacts();
-//   res.json(result);
-// });
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await Contact.find();
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-// router.get("/:contactId", async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     const result = await contacts.getContactById(contactId);
-//     if (!result) {
-//       res.status(404).json({ message: "Not found" });
-//     } else res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await Contact.findById(contactId);
+    if (!result) {
+      res.status(404).json({ message: "Not found" });
+    } else res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-// router.delete("/:contactId", async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     const result = await contacts.removeContact(contactId);
-//     if (!result) {
-//       res.status(404).json({ message: "Not found" });
-//     } else res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndDelete(contactId);
+    if (!result) {
+      res.status(404).json({ message: "Not found" });
+    } else res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post("/", async (req, res, next) => {
   try {
@@ -58,23 +68,44 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-// router.put("/:contactId", async (req, res, next) => {
-//   try {
-//     const { error } = contactPutShema.validate(req.body);
-//     if (error) {
-//       res.status(400).json({ message: "Missing fields" });
-//     }
-//     const { contactId } = req.params;
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = contactPutShema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "Missing fields" });
+    }
+    const { contactId } = req.params;
 
-//     const result = await contacts.updateContact(contactId, req.body);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
 
-//     if (!result) {
-//       res.status(404).json({ message: "Not found" });
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+    if (!result) {
+      res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { error } = contactUpdateFavoriteSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "Missing fields " });
+    }
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+    if (!result) {
+      res.status(404).json({ message: "Not found" });
+    }
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT, () => console.log("Server started! :)")))
+  .then(() =>
+    app.listen(PORT, () => console.log("Database connection successful :)"))
+  )
   .catch((err) => {
     console.error(err.message);
     process.exit(1);
